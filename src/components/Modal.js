f6dff8ec24c63a5a678b8c0e6c3a1df7b162de27
Jairo Garces Montes <jairogarces1@gmail.com>
2021-08-19 14:37:03 -0500

    add liquitoty Function

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -47,145 +47,115 @@ const Modal = ({
   const [poolLiquidity, setPoolLiquidity] = useState(1);
   const [numerator, setNumerator] = useState(1);
   const [denominator, setDenominator] = useState(1);
-  const [token0, setToken0] = useState(
-    new Token(4, pool.ids.from, 18, pool.symbols.from)
-  );
-  const [token1, setToken1] = useState(
-    new Token(4, pool.ids.to, 18, pool.symbols.to)
-  );
-  const [amount, setAmount] = useState("");
+  const [token0, setToken0] = useState(new Token(4, pool.ids.from, 18, pool.symbols.from));
+  const [token1, setToken1] = useState(new Token(4, pool.ids.to, 18, pool.symbols.to));
+  const [amount, setAmount] = useState('');
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
-
-  let toAmount, fromAmount;
-
-  if (amountInFromCurrency) {
-    fromAmount = amount;
+    
+  let toAmount, fromAmount
+  
+  if(amountInFromCurrency){
+    fromAmount = amount
     toAmount = precise(amount, numerator, denominator, token1);
   } else {
-    toAmount = amount;
-    fromAmount = precise(amount, denominator, numerator, token0);
-  }
+    toAmount = amount
+    fromAmount = precise(amount, denominator, numerator, token0)
+  }  
 
-  let dailyIncome, monthlyIncome;
+  let dailyIncome, monthlyIncome
 
   dailyIncome = calculateIncome(1);
   monthlyIncome = calculateIncome(30);
 
   useEffect(() => {
+
     // Scope for these two is local
     const fromCurrency = pool.symbols.from;
     const toCurrency = pool.symbols.to;
 
     // State setters
-    setCurrencyOptions([...Object.values(pool.symbols)]);
-    setFromCurrency(fromCurrency);
-    setToCurrency(toCurrency);
-    getMidPrice(pool).then(
-      ({
-        midPriceNum,
-        midPriceDen,
-        token0,
-        token1,
-        balance0,
-        balance1,
-        price0,
-        price1,
-        allowance0,
-        allowance1,
-        poolLiquidity,
-      }) => {
-        setNumerator(midPriceNum);
-        setDenominator(midPriceDen);
-        setToken0(token0);
-        setToken1(token1);
-        setFromBalance(balance0);
-        setToBalance(balance1);
-        setFromPrice(price0);
-        setToPrice(price1);
-        setIsFromAuthorized(allowance0);
-        setIsToAuthorized(allowance1);
-        setPoolLiquidity(poolLiquidity);
-      }
-    );
-  }, []);
+    setCurrencyOptions([...Object.values(pool.symbols)])
+    setFromCurrency(fromCurrency)
+    setToCurrency(toCurrency)
+    getMidPrice(pool).then(({midPriceNum, midPriceDen, token0, token1, balance0, balance1, price0, price1, allowance0, allowance1, poolLiquidity}) => {
+      setNumerator(midPriceNum)
+      setDenominator(midPriceDen)
+      setToken0(token0);
+      setToken1(token1);
+      setFromBalance(balance0);
+      setToBalance(balance1);
+      setFromPrice(price0);
+      setToPrice(price1);
+      setIsFromAuthorized(allowance0);
+      setIsToAuthorized(allowance1);
+      setPoolLiquidity(poolLiquidity);
+    });
+  }, []) 
 
   // Functions
-
+  
   function handleFromAmountChange(e) {
-    setAmount(e.target.value);
-    setAmountInFromCurrency(true);
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
   }
 
   function handleToAmountChange(e) {
-    setAmount(e.target.value);
-    setAmountInFromCurrency(false);
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
   }
 
   function handleAuthOnClick(e, token) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const routerAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
     const tokenContract = new ethers.Contract(
       token.address,
       [
-        "function approve(address spender, uint rawAmount) external returns (bool)",
-        "function allowance(address owner, address spender) external view returns (uint)",
-        "function balanceOf(address account) external view returns (uint)",
+      'function approve(address spender, uint rawAmount) external returns (bool)',
+      'function allowance(address owner, address spender) external view returns (uint)',
+      'function balanceOf(address account) external view returns (uint)'
       ],
       signer
     );
 
-    tokenContract
-      .approve(
-        routerAddress,
-        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-      )
-      .then((result) => {
-        provider.once(result.hash, () => {
-          if (token.symbol === token0.symbol) {
-            setIsFromAuthorized(true);
-            console.log(`Your ${token.symbol} has been approved for spending`);
-          } else if (token.symbol === token1.symbol) {
-            setIsToAuthorized(true);
-            console.log(`Your ${token.symbol} has been approved for spending`);
-          }
-        });
-      });
+    tokenContract.approve(routerAddress, '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff').then(result => {
+      provider.once(result.hash, () => {
+        if(token.symbol === token0.symbol){
+          setIsFromAuthorized(true);
+          console.log(`Your ${token.symbol} has been approved for spending`);
+        } else if (token.symbol === token1.symbol){
+          setIsToAuthorized(true);
+          console.log(`Your ${token.symbol} has been approved for spending`);
+        }
+      })
+    });
+
   }
 
-  const getMidPrice = async (pool) => {
+  const getMidPrice = async(pool) => {
+      
     // Set chain id
     const chainId = ChainId.RINKEBY;
-
+    
     // Define provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const address = await signer.getAddress();
-    const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const routerAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
     // Create token and pair instances
-    const token0 = await Fetcher.fetchTokenData(
-      chainId,
-      pool.ids.from,
-      provider,
-      pool.symbols.from
-    );
-    const token1 = await Fetcher.fetchTokenData(
-      chainId,
-      pool.ids.to,
-      provider,
-      pool.symbols.to
-    );
+    const token0 = await Fetcher.fetchTokenData(chainId, pool.ids.from, provider, pool.symbols.from);
+    const token1 = await Fetcher.fetchTokenData(chainId, pool.ids.to, provider, pool.symbols.to);
     const pair = await Fetcher.fetchPairData(token0, token1, provider);
     const route = new Route([pair], token0);
 
     const token0Contract = new ethers.Contract(
       token0.address,
       [
-        "function approve(address spender, uint rawAmount) external returns (bool)",
-        "function allowance(address owner, address spender) external view returns (uint)",
-        "function balanceOf(address account) external view returns (uint)",
+      'function approve(address spender, uint rawAmount) external returns (bool)',
+      'function allowance(address owner, address spender) external view returns (uint)',
+      'function balanceOf(address account) external view returns (uint)'
       ],
       signer
     );
@@ -193,37 +163,27 @@ const Modal = ({
     const token1Contract = new ethers.Contract(
       token1.address,
       [
-        "function approve(address spender, uint rawAmount) external returns (bool)",
-        "function allowance(address owner, address spender) external view returns (uint)",
-        "function balanceOf(address account) external view returns (uint)",
+      'function approve(address spender, uint rawAmount) external returns (bool)',
+      'function allowance(address owner, address spender) external view returns (uint)',
+      'function balanceOf(address account) external view returns (uint)'
       ],
       signer
     );
 
     const balance0Obj = await token0Contract.balanceOf(address);
-    const balance0 = parseFloat(
-      ethers.utils.formatUnits(balance0Obj.toString(), token0.decimals)
-    ).toFixed(3);
+    const balance0 = parseFloat(ethers.utils.formatUnits(balance0Obj.toString(), token0.decimals)).toFixed(3);
     const balance1Obj = await token1Contract.balanceOf(address);
-    const balance1 = parseFloat(
-      ethers.utils.formatUnits(balance1Obj.toString(), token1.decimals)
-    ).toFixed(3);
+    const balance1 = parseFloat(ethers.utils.formatUnits(balance1Obj.toString(), token1.decimals)).toFixed(3);
     let allowance0, allowance1;
 
-    if (token0.symbol !== "WETH") {
-      const allowance0Obj = await token0Contract.allowance(
-        address,
-        routerAddress
-      );
+    if(token0.symbol !== 'WETH'){
+      const allowance0Obj = await token0Contract.allowance(address, routerAddress);
       allowance0 = parseFloat(allowance0Obj.toString());
     } else {
       allowance0 = 1;
     }
-    if (token1.symbol !== "WETH") {
-      const allowance1Obj = await token1Contract.allowance(
-        address,
-        routerAddress
-      );
+    if(token1.symbol !== 'WETH') {
+      const allowance1Obj = await token1Contract.allowance(address, routerAddress);
       allowance1 = parseFloat(allowance1Obj.toString());
     } else {
       allowance1 = 1;
@@ -232,20 +192,10 @@ const Modal = ({
     const price0 = await getPrice(pool.ids.from);
     const price1 = await getPrice(pool.ids.to);
 
-    const liquidity0 = parseFloat(
-      ethers.utils.formatUnits(
-        pair.reserveOf(token0).raw.toString(),
-        token0.decimals
-      )
-    );
-    const liquidity1 = parseFloat(
-      ethers.utils.formatUnits(
-        pair.reserveOf(token1).raw.toString(),
-        token1.decimals
-      )
-    );
+    const liquidity0 = parseFloat(ethers.utils.formatUnits(pair.reserveOf(token0).raw.toString(), token0.decimals));
+    const liquidity1 = parseFloat(ethers.utils.formatUnits(pair.reserveOf(token1).raw.toString(), token1.decimals));
 
-    const poolLiquidity = liquidity0 * price0 + liquidity1 * price1;
+    const poolLiquidity = (liquidity0 * price0) + (liquidity1 * price1);
 
     // Return elements
     return {
@@ -259,59 +209,152 @@ const Modal = ({
       price1,
       allowance0,
       allowance1,
-      poolLiquidity,
-    };
-  };
-
-  function precise(amount, num, den, tk) {
-    if (amount === "") {
-      return "";
-    } else {
-      // Convert numerator and input to Fixed Number
-      const fixNum = ethers.FixedNumber.fromString(num.toString(), tk.decimals);
-      const fixAmount = ethers.FixedNumber.from(amount);
-
-      // Multiply numerator by input value, trim decimals on string representation
-      const newNum = fixNum.mulUnsafe(fixAmount);
-      const splitArr = newNum.toString().split(".");
-
-      // Replace numerator with new value
-      const newPrice = new Price(token1, token0, den, splitArr[0]);
-      return newPrice.toSignificant(6);
+      poolLiquidity
     }
   }
 
-  const getPrice = async (tokenId) => {
-    if (tokenId === "0xc778417E063141139Fce010982780140Aa0cD5Ab") {
-      let price = await axios.get(
-        "https://api.coingecko.com/api/v3/simple/price/",
-        {
-          params: {
-            ids: "ethereum",
-            vs_currencies: "usd",
-          },
-        }
-      );
-      return price.data.ethereum.usd;
+  function precise(amount, num, den, tk) {
+    if (amount === ''){
+      return '';
     } else {
-      let price = await GetPoolData.getPriceData(tokenId.toLowerCase());
-      return parseFloat(parseFloat(price[0].priceUSD).toFixed(2));
+    // Convert numerator and input to Fixed Number
+    const fixNum = ethers.FixedNumber.fromString(num.toString(), tk.decimals);
+    const fixAmount = ethers.FixedNumber.from(amount);
+    
+    // Multiply numerator by input value, trim decimals on string representation
+    const newNum = fixNum.mulUnsafe(fixAmount);
+    const splitArr = newNum.toString().split('.');
+
+    // Replace numerator with new value
+    const newPrice = new Price(token1, token0, den, splitArr[0]);
+    return newPrice.toSignificant(6)
     }
-  };
+  }
+
+  const getPrice = async(tokenId) => {
+    if(tokenId === '0xc778417E063141139Fce010982780140Aa0cD5Ab'){
+        let price = await axios.get("https://api.coingecko.com/api/v3/simple/price/", {
+            params: {
+                ids: 'ethereum',
+                vs_currencies: 'usd'
+            }
+        });
+        return price.data.ethereum.usd;
+    } else if (tokenId === '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea') {
+      let price = await axios.get("https://api.coingecko.com/api/v3/simple/price/", {
+            params: {
+                ids: 'dai',
+                vs_currencies: 'usd'
+            }
+        });
+        return  price.data.dai.usd
+    } else if (tokenId === '0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b') {
+      let price = await axios.get("https://api.coingecko.com/api/v3/simple/price/", {
+            params: {
+                ids: 'usd-coin',
+                vs_currencies: 'usd'
+            }
+        });
+        return  price.data['usd-coin'].usd;
+    } else {
+        let price = await GetPoolData.getPriceData(tokenId.toLowerCase());  
+        return parseFloat(parseFloat(price[0].priceUSD).toFixed(2));
+    }
+  }
 
   function calculateIncome(days) {
-    const myLiquidity = fromAmount * fromPrice + toAmount * toPrice;
-    const myShare = (myLiquidity / (myLiquidity + poolLiquidity)) * 100;
+    const myLiquidity = (fromAmount * fromPrice) + (toAmount * toPrice);
+    const myShare = (myLiquidity / (myLiquidity + poolLiquidity) * 100);
     const myReturn = (myShare * pool.fees24h * days).toFixed(2);
     return myReturn;
   }
 
+  const handleConfirmOnClick = async(e) => {
+
+    // Initialize variables
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+    const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const value0 = ethers.utils.parseUnits(fromAmount, token0.decimals);
+    const value1 = ethers.utils.parseUnits(toAmount, token1.decimals);
+    console.log(value0.toString());
+    console.log(value1.toString());
+
+    // Create contract instance
+    const routerContract = new ethers.Contract(
+      routerAddress,
+      [
+      'function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
+      'function addLiquidityETH(address token, uint amountTokenDesired, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity)',
+      'function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, address to, uint deadline) external returns (uint amountA, uint amountB, uint liquidity)'
+      ],
+      signer
+    );
+
+    if(token0.symbol === 'WETH') {
+
+      // ETH is in FROM position
+      const addETH = await routerContract.addLiquidityETH(
+        token1.address,
+        value1.toString(),
+        0,
+        0,
+        address,
+        deadline,
+        { value: ethers.utils.parseEther(fromAmount), gasLimit: 2000000 }
+      );
+
+      provider.once(addETH.hash, (transaction) => {
+        console.log('Your transaction was confirmed');
+        console.log(transaction);
+      });
+
+    } else if(token1.symbol === 'WETH') {
+      
+      // ETH is in TO position
+      const addETH = await routerContract.addLiquidityETH(
+        token0.address,
+        value0.toString(),
+        0,
+        0,
+        address,
+        deadline,
+        { value: ethers.utils.parseEther(toAmount), gasLimit: 2000000 }
+      );
+
+      provider.once(addETH.hash, (transaction) => {
+        console.log('Your transaction has been confirmed');
+        console.log(transaction);
+      });
+
+    } else {
+
+      // ETH is neither in TO or FROM position
+      const addLiquidity = await routerContract.addLiquidity(
+        token0.address,
+        token1.address,
+        value0,
+        value1,
+        0,
+        0,
+        address,
+        deadline
+      );
+
+      provider.once(addLiquidity.hash, () => {
+        console.log('Your transaction was confirmed');
+      })
+    }
+  }
+
   let needFromAuth = false;
   let needToAuth = false;
-  if (!isFromAuthorized) {
+  if(!isFromAuthorized) {
     needFromAuth = true;
   }
-  if (!isToAuthorized) {
+  if(!isToAuthorized) {
     needToAuth = true;
   }
 
@@ -386,9 +429,7 @@ const Modal = ({
             </div>
           ) : (
             <div>
-              <Confirm
-                onClick={(e) => console.log("This function will add liquidity")}
-              />
+              <Confirm onClick={(e) => handleConfirmOnClick(e)}/>
             </div>
           )}
           
