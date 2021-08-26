24a0ed19b7c44f388d51a6a71b13771d8c0b0892
Jairo Garces Montes <jairogarces1@gmail.com>
2021-08-26 11:36:20 -0500

    Main Chart added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Deals from "./deals";
 import Investments from "./Investments.js";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-} from "recharts";
+import ApexChart from "./mainGraph";
 
 function Home() {
   const [deals, setDeals] = useState([]);
@@ -61,52 +54,8 @@ function Home() {
     (investments) => investments.firstToken
   );
   const filteredDeals = deals.filter((deals) => deals.token0);
-  const data = [
-    {
-      day: 1,
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      day: 2,
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      day: 3,
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      
-      day: 4,
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      day: 5,
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      day: 6,
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      day: 7,
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  
+
 
   return (
     <div class="body-container">
@@ -140,30 +89,7 @@ function Home() {
         <div class="graphs-growth">
           <div class="graph-1">
             <h3>Portfolio Growth</h3>
-            <AreaChart
-              width={750}
-              height={250}
-              data={data}
-              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-            >
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                </linearGradient>
-                </defs>
-              <XAxis dataKey="day" />
-              <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip />
-              <Area
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-            </AreaChart>
+            <ApexChart/>
           </div>
         </div>
       </div>
